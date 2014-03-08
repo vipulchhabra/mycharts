9e80e805001aa963840073b9879ab041feba4c69
vipul chhabra <vipulchhabra.dc@gmail.com>
2014-03-08 11:51:13 +0530

    Initialized git and first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mycharts::Application.config.secret_key_base = '047c06f8ec02ecd196e557e63c4755b7c445f6f18d23cf928776ca9b8dff08b3c19382d2c2539029b63033eb0b32138a3c7e55e3a9313800ceb1c57a0d684c7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mycharts::Application.config.session_store :cookie_store, key: '_mycharts_session'
